68f9bdb7cbea9a42d18eb40342e889c73c7ffe22
Peter Svedman <peter.svedman@gmail.com>
2016-11-21 22:34:52 +0100

    Redrawing the board

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 // Global variables here
 var arr = [0,1,2,3,4,5,6,7,8,9];
-
+var pContents = document.querySelectorAll("p");
 // global functions here
 
-// $(document).ready(function(){ do JQuery stuff here });
+//Redraw the board
+
+function redraw(){
+    for (var i=0; i<pContents.length; i++) {
+      pContents[i].textContent = arr[i];
+    }
+}
 
 // Shorthand logger for debugging
 function logit(obj){ console.log(obj);}
@@ -11,4 +17,4 @@ function logit(obj){ console.log(obj);}
 //Shuffles an array and returns it
 
 shuffleArray(arr);
-logit(arr);
+redraw();
